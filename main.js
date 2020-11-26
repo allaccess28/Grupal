e1199885df72438a99dbc27aee8f4ef84afd7eb1
allaccess28 <68475456+allaccess28@users.noreply.github.com>
2020-11-26 13:59:08 -0300

    cambios al checklist

---
+++ main.js
@@ -0,0 +1,107 @@
+
+
+
+
+function ConfirmDemo() {
+//Ingresamos un mensaje a mostrar
+var mensaje = confirm("¿Desea validar la Entrada");
+//Detectamos si el usuario acepto el mensaje
+if (mensaje) {
+alert("¡Gracias por aceptar!")
+window.location.href = 'respondercheqlist.html';
+}
+//Detectamos si el usuario denegó el mensaje
+else {
+alert("¡Has Cancelado!");
+}
+}
+
+
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("text");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+
+    var checkBox = document.getElementById("myCheck2");
+    // Get the output text
+    var text = document.getElementById("text2");
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+    var checkBox = document.getElementById("myCheck3");
+    // Get the output text
+    var text = document.getElementById("text3");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+    var checkBox = document.getElementById("myCheck4");
+    // Get the output text
+    var text = document.getElementById("text4");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+    var checkBox = document.getElementById("myCheck5");
+    // Get the output text
+    var text = document.getElementById("text5");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+    var checkBox = document.getElementById("myCheck6");
+    // Get the output text
+    var text = document.getElementById("text6");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+    var checkBox = document.getElementById("myCheck7");
+    // Get the output text
+    var text = document.getElementById("text7");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        
+        text.style.display = "none";
+    }
+    var checkBox = document.getElementById("myCheck8");
+    // Get the output text
+    var text = document.getElementById("text8");
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        
+        text.style.display = "none";
+    }
+    /*if (checkBox.checked==false){
+        alert("Seleccione al menos una opcion");
+    }*/
+    }
